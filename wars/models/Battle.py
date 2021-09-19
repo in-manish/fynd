baa2a14f4a15f388882ad96437c2e3acc81f7b14
Manish <sonal@fairfest.in>
2021-09-19 13:26:25 +0530

    Battle model fields changed

--- wars/models/Battle.py
+++ wars/models/Battle.py
@@ -11,21 +11,29 @@ class Battle(models.Model):
     """
     title = models.CharField(max_length=200)
     year = models.PositiveSmallIntegerField()
+    battle_number = models.PositiveIntegerField(unique=True, db_index=True)
     attacker_king = models.ForeignKey(
         Warrior, related_name='attacker_king_battles', on_delete=models.CASCADE, null=True
     )
     defender_king = models.ForeignKey(
         Warrior, related_name='defender_king_battles', on_delete=models.CASCADE, null=True
     )
+    attacker_commanders = models.ManyToManyField(
+        Warrior, related_name='attacker_commanders_battles'
+    )
+    defender_commanders = models.ManyToManyField(
+        Warrior, related_name='defender_commanders_battles'
+    )
+    is_attacker_win = models.NullBooleanField(default=None)
     attackers = models.ManyToManyField(Warrior, related_name='attackers_battles')
     defenders = models.ManyToManyField(Warrior, related_name='defenders_battles')
     battle_type = models.ForeignKey(BattleType, related_name='battle_type_battles', on_delete=models.SET_NULL,
                                     null=True)
-    death = models.PositiveSmallIntegerField(default=0)
-    capture = models.PositiveSmallIntegerField(default=0)
-    attacker_size = models.PositiveSmallIntegerField(default=0)
-    defender_size = models.PositiveSmallIntegerField(default=0)
-    is_summer = models.BooleanField(default=False)
+    death = models.PositiveIntegerField(default=0, null=True)
+    capture = models.PositiveIntegerField(default=0, null=True)
+    attacker_size = models.PositiveIntegerField(default=0, null=True)
+    defender_size = models.PositiveIntegerField(default=0, null=True)
+    is_summer = models.BooleanField(default=False, null=True)
     location = models.ForeignKey(Location, related_name='location_battles', on_delete=models.SET_NULL, null=True)
     note = models.TextField(default=str, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- wars/models/Region.py
+++ wars/models/Region.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class Region(models.Model):
     title = models.CharField(max_length=70, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
