8df85ff50a244e59c49ef84861986e0aa376cc20
Manish <sonal@fairfest.in>
2021-10-01 17:56:03 +0530

    battletype title and region title made unique and migration file added

--- wars/models/BattleType.py
+++ wars/models/BattleType.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class BattleType(models.Model):
-    title = models.CharField(max_length=50, db_index=True)
+    title = models.CharField(unique=True, max_length=50, db_index=True)
--- wars/models/Region.py
+++ wars/models/Region.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Region(models.Model):
-    title = models.CharField(max_length=70, db_index=True)
+    title = models.CharField(unique=True, max_length=70, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
